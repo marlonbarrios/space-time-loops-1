19353a83fa36841c433086326687f7fed54cc310
marlonbarrios <marlon@dance-tech.net>
2022-06-02 14:13:00 -0400

    with music

--- sketch.js
+++ sketch.js
@@ -7,15 +7,39 @@ var video;
 var button;
 var snapShots = [];
 var counter = 0;
-total = 131;
+var total = 131;
+var song;
+
 
 function setup() {
   createCanvas(800, 600);
+  song = loadSound("canon-in-d.mp3", loaded)
+ 
   video = createCapture(VIDEO, ready);
   image(video, 0,0);
+  button = createButton("Play  Pachebel Cannon in D");
+  button.mousePressed(togglePlaying);
   background(0);
   video.size(320, 240);
+ 
+ 
+}
+
+function togglePlaying(){
+  if (!song.isPlaying()) {
+    song.play();
+    song.setVolume(0.5);
   
+
+    button.html('Stop')
+  } else {
+    song.stop();
+    button.html('Start Again')
+  }
+}
+
+function loaded() {
+  console.log('loaded')
 }
 
 var go = false;
@@ -37,6 +61,9 @@ function draw() {
     }
   }
   
+  let videoRecorder;
+
+
   var  w = 80;
   var  h = 60;
   var  x = 0;
@@ -53,6 +80,3 @@ function draw() {
     }
   }
 }
-
-
-
